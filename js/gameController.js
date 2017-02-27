a8cb94dac0092f8da64a611880efa55da2b7479b
ksleano <ksleano@gmail.com>
2017-02-27 10:27:31 -0800

    Added the computer choice function

--- js/gameController.js
+++ js/gameController.js
@@ -11,7 +11,8 @@
         // Variables
         $scope.message = "";
         $scope.showMessage = false;
-
+        
+        // user2 is comp
     	$scope.user2icon = "img/user2icon.png";
         $scope.options = [ {key: "rock", value: "img/rock.png"}, {key: "paper", value: "img/paper.png"}, {key: "scissors", value: "img/scissors.png"}];
 
@@ -21,19 +22,42 @@
                 case "rock":
                     $scope.showMessage = true;
                     $scope.message = "rock clicked!";
+                    $scope.generateCompChoice()
                     break;
                 case "paper":
                     $scope.showMessage = true;
                     $scope.message = "paper clicked!";
+                    $scope.generateCompChoice()
                     break;
                 case "scissors":
                     $scope.showMessage = true;
+                    $scope.generateCompChoice();
                     $scope.message = "scissors clicked!";
                     break;
                 case "default":
             }
     	}
 
+    	//comp choice function
+        $scope.generateCompChoice = function(){
+    	    // random number from 1-3.
+    	    var num = Math.floor(Math.random()*3 + 1);
+    	    console.log(num);
+    	    switch (num) {
+                case 1:
+                    $scope.user2icon = "img/rock.png";
+                    break;
+                case 2:
+                    $scope.user2icon = "img/paper.png";
+                    break;
+                case 3:
+                    $scope.user2icon = "img/scissors.png";
+                    break
+            }
+        }
+        
     }
+    
+
 
 })();
