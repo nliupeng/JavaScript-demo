d115e423cf30dcfa2f96adc57154036c6819290e
nliupeng <nliupeng@gmail.com>
2017-02-27 14:28:38 -0800

    added calculateResult function

--- js/gameController.js
+++ js/gameController.js
@@ -11,53 +11,71 @@
         // Variables
         $scope.message = "";
         $scope.showMessage = false;
+        const rock = 0;
+        const paper = 1;
+        const scissors = 2;
         
-        // user2 is comp
-    	$scope.user2icon = "img/user2icon.png";
-        $scope.options = [ {key: "rock", value: "img/rock.png"}, {key: "paper", value: "img/paper.png"}, {key: "scissors", value: "img/scissors.png"}];
+    	$scope.compIcon = "img/unknown.png";
+        $scope.options = [ {key: rock, value: "img/rock.png"}, {key: paper, value: "img/paper.png"}, {key: scissors, value: "img/scissors.png"}];
+        $scope.resultMessage = "READY?";
 
-    	$scope.selectOption = function(key) {
-    		// when user clicks on icon, display computer's result
-            switch(key) {
-                case "rock":
-                    $scope.showMessage = true;
-                    $scope.message = "rock clicked!";
-                    $scope.generateCompChoice()
-                    break;
-                case "paper":
-                    $scope.showMessage = true;
-                    $scope.message = "paper clicked!";
-                    $scope.generateCompChoice()
-                    break;
-                case "scissors":
-                    $scope.showMessage = true;
-                    $scope.generateCompChoice();
-                    $scope.message = "scissors clicked!";
-                    break;
-                case "default":
-            }
-    	}
+        $scope.calculateResult = function(userChoice) {
+            var compChoice = generateCompChoice();
+
+            if (userChoice === compChoice) {
+                $scope.resultMessage = "It's a draw!";
+            } 
+            else {
+                switch(userChoice) {
+                    case rock:
+                        if (compChoice === paper) {
+                            $scope.resultMessage = "Computer wins!";
+                        } 
+                        else {
+                            $scope.resultMessage = "You win!";
+                        }
+                        break;
+                    case paper:
+                        if (compChoice === rock) {
+                            $scope.resultMessage = "You win!";
+                        } 
+                        else {
+                            $scope.resultMessage = "Computer wins!";
+                        }
+                        break;
+                    case scissors:
+                        if (compChoice === rock) {
+                            $scope.resultMessage = "Computer wins!";
+                        } 
+                        else {
+                            $scope.resultMessage = "You win!";
+                        }
+                        break;
+                } // end switch
+            } // end else
+        }
 
     	//comp choice function
-        $scope.generateCompChoice = function(){
-    	    // random number from 1-3.
-    	    var num = Math.floor(Math.random()*3 + 1);
-    	    console.log(num);
+        var generateCompChoice = function(){
+    	    var num = Math.floor(Math.random()*3);
+    	    console.log("Computer's choice: " + num);
+
     	    switch (num) {
-                case 1:
-                    $scope.user2icon = "img/rock.png";
+                case rock:
+                    $scope.compIcon = "img/rock.png";
+                    return rock;
                     break;
-                case 2:
-                    $scope.user2icon = "img/paper.png";
+                case paper:
+                    $scope.compIcon = "img/paper.png";
+                    return paper;
                     break;
-                case 3:
-                    $scope.user2icon = "img/scissors.png";
-                    break
+                case scissors:
+                    $scope.compIcon = "img/scissors.png";
+                    return scissors;
+                    break;
             }
         }
         
     }
     
-
-
 })();
