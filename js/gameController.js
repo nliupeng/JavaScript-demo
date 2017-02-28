603142b6a30cb611dbcc87b1f74f260e54619572
ksleano <ksleano@gmail.com>
2017-02-27 22:24:37 -0800

    Added 3 img and 3 sfx for said img

--- js/gameController.js
+++ js/gameController.js
@@ -11,6 +11,7 @@
         // Variables
         $scope.message = "";
         $scope.showMessage = false;
+
         
         // user2 is comp
     	$scope.user2icon = "img/user2icon.png";
@@ -23,16 +24,21 @@
                     $scope.showMessage = true;
                     $scope.message = "rock clicked!";
                     $scope.generateCompChoice()
+                    $scope.playAudio(key, 0);
                     break;
                 case "paper":
                     $scope.showMessage = true;
                     $scope.message = "paper clicked!";
                     $scope.generateCompChoice()
+                    $scope.playAudio(key, 1);
+
                     break;
                 case "scissors":
                     $scope.showMessage = true;
                     $scope.generateCompChoice();
                     $scope.message = "scissors clicked!";
+                    $scope.playAudio(key, 2);
+
                     break;
                 case "default":
             }
@@ -42,18 +48,26 @@
         $scope.generateCompChoice = function(){
     	    // random number from 1-3.
     	    var num = Math.floor(Math.random()*3 + 1);
-    	    console.log(num);
     	    switch (num) {
                 case 1:
-                    $scope.user2icon = "img/rock.png";
+                    // same as $scope.user2icon = "img/rock.png";
+
+                    $scope.user2icon = $scope.options[0].value;
                     break;
                 case 2:
-                    $scope.user2icon = "img/paper.png";
+                    $scope.user2icon = $scope.options[1].value;
                     break;
                 case 3:
-                    $scope.user2icon = "img/scissors.png";
+                    $scope.user2icon = $scope.options[2].value;
                     break
             }
+        }
+        //////////////// End function ////////////////
+
+        //audio confirmation
+        $scope.playAudio = function(key, i){
+            var audio = new Audio('audio/sfx' +$scope.options[i].key + '.mp3');
+            audio.play();
         }
         
     }
