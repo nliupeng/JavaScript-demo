caa7f1d892511178c9cb0404aaa539a582543ace
nliupeng <nliupeng@gmail.com>
2017-03-01 11:45:45 -0800

    fixed merge conflicts

--- js/gameController.js
+++ js/gameController.js
@@ -9,74 +9,97 @@
 
     function gameController($scope) {
         // Variables
-        $scope.message = "";
-        $scope.showMessage = false;
+        const rock = 0;
+        const paper = 1;
+        const scissors = 2;
+        
+        $scope.compIcon = "img/unknown.png";
+        $scope.options = [ {key: rock, value: "img/rock.png"}, {key: paper, value: "img/paper.png"}, {key: scissors, value: "img/scissors.png"}];
+        $scope.resultMessage = "READY?";
+        $scope.userScore = 0;
+        $scope.compScore = 0;
 
-<<<<<<< HEAD
-=======
-        // user2 is comp
->>>>>>> 3ded33762198027fe1631a3d77dabd3baae6485c
-    	$scope.user2icon = "img/user2icon.png";
-        $scope.options = [ {key: "rock", value: "img/rock.png"}, {key: "paper", value: "img/paper.png"}, {key: "scissors", value: "img/scissors.png"}];
+        /* Calculate winner of round */
+        $scope.calculateResult = function(userChoice) {
+            $scope.playAudio(userChoice);
 
-    	$scope.selectOption = function(key) {
-    		// when user clicks on icon, display computer's result
-            switch(key) {
-                case "rock":
-                    $scope.showMessage = true;
-                    $scope.message = "rock clicked!";
-<<<<<<< HEAD
-=======
-                    $scope.generateCompChoice()
-                    $scope.playAudio(key, 0);
->>>>>>> 3ded33762198027fe1631a3d77dabd3baae6485c
-                    break;
-                case "paper":
-                    $scope.showMessage = true;
-                    $scope.message = "paper clicked!";
-<<<<<<< HEAD
-                    break;
-                case "scissors":
-                    $scope.showMessage = true;
-                    $scope.message = "scissors clicked!";
-=======
-                    $scope.generateCompChoice()
-                    $scope.playAudio(key, 1);
-                    break;
-                case "scissors":
-                    $scope.showMessage = true;
-                    $scope.generateCompChoice();
-                    $scope.message = "scissors clicked!";
-                    $scope.playAudio(key, 2);
->>>>>>> 3ded33762198027fe1631a3d77dabd3baae6485c
-                    break;
-                case "default":
-            }
-    	}
+            var compChoice = generateCompChoice();
 
-    	//comp choice function
-        $scope.generateCompChoice = function(){
-    	    // random number from 1-3.
-    	    var num = Math.floor(Math.random()*3 + 1);
-    	    console.log(num);
-    	    switch (num) {
-                case 1:
-                    $scope.user2icon = "img/rock.png";
-                    break;
-                case 2:
-                    $scope.user2icon = "img/paper.png";
-                    break;
-                case 3:
-                    $scope.user2icon = "img/scissors.png";
-                    break
-            }
+            if (userChoice === compChoice) {
+                $scope.resultMessage = "It's a draw!";
+                $scope.userScore++;
+                $scope.compScore++;
+            } 
+            else {
+                switch(userChoice) {
+                    case rock:
+                        if (compChoice === paper) {
+                            $scope.resultMessage = "Computer wins!";
+                            $scope.compScore++;
+                        } 
+                        else {
+                            $scope.resultMessage = "You win!";
+                            $scope.userScore++;
+                        }
+                        break;
+                    case paper:
+                        if (compChoice === rock) {
+                            $scope.resultMessage = "You win!";
+                            $scope.userScore++;
+                        } 
+                        else {
+                            $scope.resultMessage = "Computer wins!";
+                            $scope.compScore++;
+                        }
+                        break;
+                    case scissors:
+                        if (compChoice === rock) {
+                            $scope.resultMessage = "Computer wins!";
+                            $scope.compScore++;
+                        } 
+                        else {
+                            $scope.resultMessage = "You win!";
+                            $scope.userScore++;
+                        }
+                        break;
+                } // end switch
+            } // end else
         }
 
-        // audio
-        $scope.playAudio = function(key, i){
-            var audio = new Audio('audio/sfx' +$scope.options[i].key + '.mp3');
+        /* Comp choice function */
+        var generateCompChoice = function(){
+            var num = Math.floor(Math.random()*3);
+            console.log("Computer's choice: " + num);
+
+            switch (num) {
+                case rock:
+                    $scope.compIcon = "img/rock.png";
+                    return rock;
+                    break;
+                case paper:
+                    $scope.compIcon = "img/paper.png";
+                    return paper;
+                    break;
+                case scissors:
+                    $scope.compIcon = "img/scissors.png";
+                    return scissors;
+                    break;
+            }
+        }
+        
+        /* Reset game */
+        $scope.reset = function() {
+            $scope.resultMessage = "READY?";
+            $scope.userScore = 0;
+            $scope.compScore = 0;
+        }
+
+        /* Audio function */
+        $scope.playAudio = function(i) {
+            var audio = new Audio('audio/sfx' + i + '.mp3');
             audio.play();
         }
 
-    }
+    } // end gameController()
+    
 })();
